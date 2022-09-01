76b6954fb9975dedcdae397ffaa0ab39a683916d
Sadik <sadikvkm123@gmail.com>
2022-09-02 00:33:48 +0530

    list

--- lib/modules/my_trips/methods/trip_methods.dart
+++ lib/modules/my_trips/methods/trip_methods.dart
@@ -1,13 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:grouptravel/modules/my_trips/models/trip_list_model.dart';
+import 'package:grouptravel/modules/my_trips/models/Job.dart';
 import 'package:grouptravel/services/request.dart';
+import 'package:http/http.dart' as http;
 
 Future createTrip(data) async {
   final result = await await httpRequest('/trips', data, 'POST');
   return result;
 }
 
-Future getAllTrips() async {
+Future<List<Job>> getAllTrips() async {
+  const jobsListAPIUrl = 'https://mock-json-service.glitch.me/';
+  final response = await http.get(Uri.parse(jobsListAPIUrl));
+
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((job) => Job.fromJson(job)).toList();
+  } else {
+    throw Exception('Failed to load jobs from API');
+  }
   final result = await httpRequest('/trips');
   return result;
 }
--- lib/modules/my_trips/trips_list.dart
+++ lib/modules/my_trips/trips_list.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'dart:developer';
 
-import 'package:grouptravel/modules/my_trips/models/trip_list_model.dart';
+import 'package:grouptravel/modules/my_trips/models/Job.dart';
 import 'package:grouptravel/modules/my_trips/new_trip.dart';
 import 'package:grouptravel/modules/my_trips/trip_map.dart';
 import 'package:grouptravel/widget/page_container.dart';
@@ -15,21 +16,6 @@ class MyTrips extends StatefulWidget {
 
 class MyTripsState extends State<MyTrips> {
 
-  List<TripListModel> _newsArticles = <TripListModel>[];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchInitialData();
-  }
-
-  void fetchInitialData() async {
-
-    final d = await getAllTrips();
-    _newsArticles = d['result'];
-
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -43,29 +29,40 @@ class MyTripsState extends State<MyTrips> {
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
       ),
-      child: ListView.builder(
-        itemCount: _newsArticles.length,
-        itemBuilder: (context, index) {
-          return Card(
-
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TripMap()));
-              },
-              child: Column(
-                children: <Widget>[
-                  Text('$_newsArticles'),
-                  const ListTile(
-                    leading: Icon(Icons.flight),
-                    title: Text('Trip to Paris'),
-                    subtitle: Text('Departure: 12/12/2019'),
-                  ),
-                ],
-              ),
-            ),
-          );
+      child: FutureBuilder<List<Job>>(
+        future: getAllTrips(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Job>? data = snapshot.data;
+            return _jobsListView(data);
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return const CircularProgressIndicator();
         },
       ),
     );
   }
 }
+
+
+ListView _jobsListView(data) {
+  return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return _tile(data[index].position, data[index].company, Icons.work);
+      });
+}
+
+ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+  title: Text(title,
+      style: const TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 20,
+      )),
+  subtitle: Text(subtitle),
+  leading: Icon(
+    icon,
+    color: Colors.blue[500],
+  ),
+);
